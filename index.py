007fc87e27ebb1ff0b2e0da41195eb1b9ab20361
Arnold <Array0824@gmail.com>
2023-12-13 23:42:56 -0500

    first commit

---
+++ index.py
@@ -0,0 +1,67 @@
+
+#Exercise 1
+#Given an array of positive integers nums, return a list of all of the negative integers.
+
+
+nums = [1, 3, 5, 7, 8]
+new_list = []
+
+for num in nums:
+    negative_list = (nums.inrange(leng)[0,-9]):
+    new_nums = negative_list
+    return new_nums + new_list.append
+
+print(new_list)
+
+Ex. 1
+nums = [1, 3, 5, 7, 8]
+Expected Output: [-1, -3, -5, -7, -8]
+
+Ex. 2
+nums = [100, 534, 32, 15, 77, 222, 788, 345, 75645, 22]
+Expected Output: [-100, -534, -32, -15, -77, -222, -788, -345, -75645, -22]
+
+
+
+
+
+#Exercise 2
+#Given a string, return a list of all of the digits in the string.
+
+address = "123 Real Street, Apt. 2, Springfield, OR 43498"
+
+for str in address:
+    if str == int
+    return int
+
+
+
+Ex. 1
+address = "123 Real Street, Apt. 2, Springfield, OR 43498"
+Expected Output: ['1', '2', '3', '2', '4', '3', '4', '9', '8']
+
+Ex. 2
+sentence = "My phone number is (555) 555-4321"
+Expected Output: ['5', '5', '5', '5', '5', '5', '4', '3', '2', '1']
+
+
+
+
+#Exercise 3
+#Given a string digits, return a string of the digits + 1
+
+digits = '123'
+new_string = ("")
+
+for str in digits 
+return  str + 1 in new_string.append 
+
+
+Ex. 1
+digits = '123'
+Expected Output: '124'
+
+Ex. 2
+digits = '99'
+Expected Output: '100'
+
